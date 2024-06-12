9c452b16d82931ddf2f6e27990bed564b54f6f17
ThibautCadiou <cadioucontact@gmail.com>
2024-06-12 11:32:03 +0200

    Toujours probleme fermeture animation

---
+++ src/components/Paragraphe.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+
+export default function Paragraphe(props) {
+    // {isOpen && <div className="collapse__content collapse--show"></div>}
+    console.log(props.text);
+}
