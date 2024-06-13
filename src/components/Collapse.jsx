ce9e9fb1b731ef1c9a73f0a5cc6896596a8cf8a8
ThibautCadiou <cadioucontact@gmail.com>
2024-06-13 15:33:26 +0200

    correction du bug sur le underlined avec un !important

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -9,27 +9,46 @@ export default function Collapse(props) {
 
     const toggleCollapse = () => {
         setIsOpen(!isOpen);
+        if (!isOpen) {
+            setisAnimationFinished(false);
+        } else {
+            setTimeout(() => {
+                setisAnimationFinished(true);
+            }, 200);
+        }
+
         if (isFirstOpening) {
             setIsFirstOpening(false);
         }
-
-        setTimeout(() => {
-            setisAnimationFinished(true);
-        }, 3000); // 300ms delay
     };
 
-    return (
-        <div className="collapse">
-            <div className="collapse__header">
-                <span className="collapse__header__title">{props.obj.title}</span>
-                {isFirstOpening ? (
-                    <i className="fa-solid fa-chevron-up" onClick={toggleCollapse}></i>
-                ) : (
-                    <i onClick={toggleCollapse} className={isOpen ? "fa-solid fa-chevron-down collapse__icon--open" : "fa-solid fa-chevron-up collapse__icon--close"}></i>
-                )}
+    if (isAnimationFinished) {
+        return (
+            <div className="collapse">
+                <div className="collapse__header">
+                    <span className="collapse__header__title">{props.obj.title}</span>
+                    {isFirstOpening ? (
+                        <i className="fa-solid fa-chevron-up" onClick={toggleCollapse}></i>
+                    ) : (
+                        <i onClick={toggleCollapse} className={isOpen ? "fa-solid fa-chevron-down collapse__icon--open" : "fa-solid fa-chevron-up collapse__icon--close"}></i>
+                    )}
+                </div>
             </div>
+        );
+    } else {
+        return (
+            <div className="collapse">
+                <div className="collapse__header">
+                    <span className="collapse__header__title">{props.obj.title}</span>
+                    {isFirstOpening ? (
+                        <i className="fa-solid fa-chevron-up" onClick={toggleCollapse}></i>
+                    ) : (
+                        <i onClick={toggleCollapse} className={isOpen ? "fa-solid fa-chevron-down collapse__icon--open" : "fa-solid fa-chevron-up collapse__icon--close"}></i>
+                    )}
+                </div>
 
-            {isOpen ? <div className="collapse__content collapse--show">{props.obj.parapgraphe}</div> : <div className={isFirstOpening ? "hide" : "collapse__content collapse--hide"}>{props.obj.parapgraphe}</div>}
-        </div>
-    );
+                {isOpen ? <div className="collapse__content collapse--show">{props.obj.parapgraphe}</div> : <div className={isFirstOpening ? "hide" : "collapse__content collapse--hide"}>{props.obj.parapgraphe}</div>}
+            </div>
+        );
+    }
 }
--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,14 +1,39 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
+
+import { useState } from "react";
 
 export default function PageNav() {
+    const [myPath, setMyPath] = useState("/");
+
+    const sayLocation = (a) => {
+        setTimeout(() => {
+            setMyPath(a.pathname);
+            console.log(myPath);
+        }, 50);
+
+        if (myPath === "/") {
+            console.log("yes");
+        } else {
+            console.log("no");
+        }
+    };
+
+    // const sayHello =()=>{
+    //     console.log("Hello");
+    // }
+
     return (
         <nav>
             <ul className="navigation">
-                <li className="navigation-link">
-                    <NavLink to="/"> Accueil </NavLink>
+                <li className="navigation-link" onClick={sayLocation(useLocation())}>
+                    <NavLink to="/" className={myPath === "/" ? "underlined" : ""}>
+                        Accueil
+                    </NavLink>
                 </li>
-                <li className="navigation-link">
-                    <NavLink to="/propos">A Propos</NavLink>
+                <li className="navigation-link" onClick={sayLocation(useLocation())}>
+                    <NavLink to="/propos" className={myPath === "/propos" ? "underlined" : ""}>
+                        A Propos
+                    </NavLink>
                 </li>
             </ul>
         </nav>
