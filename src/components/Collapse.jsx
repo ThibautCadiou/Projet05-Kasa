12a8b338d73917fba701268b2d54fc4ce5cd07bc
ThibautCadiou <cadioucontact@gmail.com>
2024-06-13 12:31:49 +0200

    juste avant tout casser v2

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -5,12 +5,17 @@ import React, { useState } from "react";
 export default function Collapse(props) {
     const [isOpen, setIsOpen] = useState(false);
     const [isFirstOpening, setIsFirstOpening] = useState(true);
+    const [isAnimationFinished, setisAnimationFinished] = useState(false);
 
     const toggleCollapse = () => {
         setIsOpen(!isOpen);
         if (isFirstOpening) {
             setIsFirstOpening(false);
         }
+
+        setTimeout(() => {
+            setisAnimationFinished(true);
+        }, 3000); // 300ms delay
     };
 
     return (
@@ -24,7 +29,7 @@ export default function Collapse(props) {
                 )}
             </div>
 
-            {isOpen && <div className="collapse__content collapse--show">{props.obj.parapgraphe}</div>}
+            {isOpen ? <div className="collapse__content collapse--show">{props.obj.parapgraphe}</div> : <div className={isFirstOpening ? "hide" : "collapse__content collapse--hide"}>{props.obj.parapgraphe}</div>}
         </div>
     );
 }
