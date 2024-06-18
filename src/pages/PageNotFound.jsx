e5cf54c74c73fc6126932c404667dd988e5f4d88
ThibautCadiou <cadioucontact@gmail.com>
2024-06-18 10:46:05 +0200

    Finalisation de la page erreur

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 export default function PageNotFound() {
     return (
         <div className="error-page">
-            <img className="error-page_logo" src="src/assets/404.png" alt="Logo error 404" />
+            <img className="error-page__logo" src="src/assets/404.png" alt="Logo error 404" />
             <p className="error-page__second-message">Oups! La page que vous demandez n&apos;existe pas.</p>
             <NavLink to="/" className="error-page__back-link">
                 Retourner sur la page d&apos;accueil
